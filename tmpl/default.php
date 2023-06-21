b5d47b86c77a668ea8faa113d8a8eafcf976d317
Russell English <russell.english@stormit.co.uk>
2023-06-21 19:09:55 +0100

    Updated to match tester.co.uk

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,99 +12,22 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 
 ?>
-
-
 <div class="jl-position-relative jl-visible-toggle" tabindex="-1" jl-slideshow="autoplay: true; animation: scale; max-height:580">
-
     <ul class="jl-slideshow-items">
         <?php foreach ($slides as $slide) : ?>
-
-            <?php
-            if ($slide->text_align == '0') {
-                $align = 'left';
-            } else {
-                $align = 'right';
-            }
-            ?>
-
-            <?php
-            if ($slide->text_align == '0') {
-                $align = 'jl-position-top-left';
-            }
-            if ($slide->text_align == '1') {
-                $align = 'jl-position-top-center';
-            }
-            if ($slide->text_align == '2') {
-                $align = 'jl-position-top-right';
-            }
-            if ($slide->text_align == '3') {
-                $align = 'jl-position-center';
-            }
-            if ($slide->text_align == '4') {
-                $align = 'jl-position-center-left';
-            }
-            if ($slide->text_align == '5') {
-                $align = 'jl-position-center-right';
-            }
-            if ($slide->text_align == '6') {
-                $align = 'jl-position-bottom-left';
-            }
-            if ($slide->text_align == '7') {
-                $align = 'jl-position-bottom-center';
-            }
-            if ($slide->text_align == '8') {
-                $align = 'jl-position-bottom-right';
-            } ?>
             <li>
-
-                <?php if ($slide->background_type == '0') { ?>
-                    <img src="<?php echo $slide->background_image; ?>" alt="<?php echo $slide->title; ?>" jl-contain>
-                <?php }
-                if ($slide->background_type == '1') { ?>
-                    <video src="images/<?php echo $slide->background_video; ?>" loop muted playsinline jl-cover jl-video="autoplay: inview"></video>
-                <?php } 
-                if ($slide->background_type == '2') { ?>
-                    <iframe src="https://www.youtube-nocookie.com/embed/<?php echo $slide->youtube;?>?autoplay=0&amp;showinfo=0&amp;rel=0&amp;modestbranding=1&amp;playsinline=1" width="1920" height="1080" frameborder="0" allowfullscreen jl-responsive jl-video="automute: true"></iframe>
-                <?php }
-
-                if ($slide->background_type == '3') { ?>
-                    <div style="<?php echo $slide->backgroundcolor; ?>"></div>
-                <?php } ?>
-
-                <?php if ($slide->show_overlay == '1') { ?>
-
-                <div class="<?php echo $align; ?> jl-text-left jl-width-1-4@l jl-overlay jl-overlay-default ">
-                    <div>
-                        <div class="jl-margin-remove jl-h2 jl-text-bold text-black"><?php echo $slide->title; ?></div>
-                        <div class="jl-margin-remove jl-content text-black"><?php echo $slide->sub_title; ?></div>
-                        <p jl-margin>
-                            <a class="jl-button jl-button-primary" href="<?php
-                                                                            if ($slide->link_type == '0') {
-                                                                                echo JRoute::_("index.php?Itemid={$slide->menuitem}");
-                                                                            } else {
-                                                                                echo $slide->link;
-                                                                            } ?>">
-                                <?php echo $slide->button_text; ?>
-                            </a>
-                        </p>
+                <div style="background-color:<?php echo $slide->backgroundcolor; ?>" jl-grid>
+                    <div class="jl-width-2-3">
+                        <img src="<?php echo $slide->client_logo; ?>" alt="" />
+                        <h2><?php echo $slide->title; ?></h2>
+                        <?php echo $slide->description; ?>
+                        <a class="jl-button jl-button-default"><a href="<?php echo $slide->link_type; ?>"><?php echo $slide->button; ?></a>
+                    </div>
+                    <div class="jl-width-1-3">
+                        <img src="<?php echo $slide->product_image_one; ?>" alt="<?php echo $slide->title; ?>" />
+                        <?php echo $slide->new_product; ?>
                     </div>
                 </div>
-
-                <?php }
-
-                if ($slide->background_type == '3') { ?>
-
-<div style="background-color:<?php echo $slide->backgroundcolor; ?>" jl-grid class="jl-child-width-1-3">
-    <div><img src="<?php echo $slide->buildit_left_image;?>"/></div>
-    <div class="jl-text-center jl-margin-top">
-        <div><img src="<?php echo $slide->buildit_center_image;?>"/></div>
-        <div><?php echo $slide->buildit_text;?></div>
-    </div>
-    <div><img src="<?php echo $slide->buildit_right_image;?>"/></div>
-</div>
-
-                <?php  } ?>
-
             </li>
         <?php endforeach; ?>
     </ul>
@@ -113,5 +36,4 @@ use Joomla\CMS\HTML\HTMLHelper;
         <a class="jl-position-center-left jl-position-small jl-hidden-hover" href="#" jl-slidenav-previous jl-slideshow-item="previous"></a>
         <a class="jl-position-center-right jl-position-small jl-hidden-hover" href="#" jl-slidenav-next jl-slideshow-item="next"></a>
     </div>
-
 </div>
