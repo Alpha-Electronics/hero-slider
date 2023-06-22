0f66d18f81bce44163a141201f75b3059ec55cb4
Russell English <russell.english@stormit.co.uk>
2023-06-22 13:29:29 +0100

    Updated

--- tmpl/default.php
+++ tmpl/default.php
@@ -24,7 +24,7 @@ use Joomla\CMS\HTML\HTMLHelper;
             endif; ?>
 
             <?php if ($slide->new_product == 1) :
-                $newproduct = 'New Product!';
+                $newproduct = '<div class="jl-text-large jl-text-uppercase">New</div>Product!';
             else :
                 $newproduct = '';
             endif; ?>
