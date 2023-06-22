0bd85708b6d5a99e4e4b2ecbb71f4741246bd575
Russell English <russell.english@stormit.co.uk>
2023-06-22 12:36:38 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,53 +12,36 @@ defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\HTML\HTMLHelper;
 
 ?>
-<div class="jl-position-relative jl-visible-toggle" tabindex="-1" jl-slideshow="autoplay: true; animation: scale; max-height:580" style="background-color:<?php echo $slide->backgroundcolor; ?>">
+
+<div class="jl-position-relative jl-visible-toggle" tabindex="-1" jl-slideshow="autoplay: true; animation: scale; max-height:580">
+
+    <div class="jl-full-width-background" style="background-color:<?php echo $slides[0]->backgroundcolor; ?>"></div>
     <div class="jl-container">
-        <ul class="jl-slideshow-items">
-            <?php foreach ($slides as $slide) : ?>
-                <li>
-                    <div jl-grid>
-                        <div class="jl-width-2-3">
-                            <img src="<?php echo $slide->client_logo; ?>" alt="" />
-                            <h2><?php echo $slide->title; ?></h2>
-                            <?php echo $slide->description; ?>
-                            <a class="jl-button jl-button-default"><a href="<?php echo $slide->link_type; ?>"><?php echo $slide->button; ?></a>
+        <div class="jl-slideshow-wrapper">
+            <ul class="jl-slideshow-items">
+                <?php foreach ($slides as $slide) : ?>
+                    <li>
+                        <div class="jl-position-relative jl-visible-toggle" tabindex="-1" jl-slideshow="autoplay: true; animation: scale; max-height:580">
+                            <div jl-grid>
+                                <div class="jl-width-2-3">
+                                    <img src="<?php echo $slide->client_logo; ?>" alt="" />
+                                    <h2><?php echo $slide->title; ?></h2>
+                                    <?php echo $slide->description; ?>
+                                    <a class="jl-button jl-button-default" href="<?php echo $slide->link_type; ?>"><?php echo $slide->button; ?></a>
+                                </div>
+                                <div class="jl-width-1-3">
+                                    <img src="<?php echo $slide->product_image_one; ?>" alt="<?php echo $slide->title; ?>" />
+                                    <?php echo $slide->new_product; ?>
+                                </div>
+                            </div>
                         </div>
-                        <div class="jl-width-1-3">
-                            <img src="<?php echo $slide->product_image_one; ?>" alt="<?php echo $slide->title; ?>" />
-                            <?php echo $slide->new_product; ?>
-                        </div>
-                    </div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </div>
     <div class="jl-dark">
         <a class="jl-position-center-left jl-position-small jl-hidden-hover" href="#" jl-slidenav-previous jl-slideshow-item="previous"></a>
         <a class="jl-position-center-right jl-position-small jl-hidden-hover" href="#" jl-slidenav-next jl-slideshow-item="next"></a>
     </div>
-</div>
-
-
-<div class="jl-container">
-    <ul class="jl-slideshow-items">
-        <?php foreach ($slides as $slide) : ?>
-            <div class="jl-position-relative jl-visible-toggle" tabindex="-1" jl-slideshow="autoplay: true; animation: scale; max-height:580" style="background-color:<?php echo $slides[0]->backgroundcolor; ?>">
-                <li>
-                    <div jl-grid>
-                        <div class="jl-width-2-3">
-                            <img src="<?php echo $slide->client_logo; ?>" alt="" />
-                            <h2><?php echo $slide->title; ?></h2>
-                            <?php echo $slide->description; ?>
-                            <a class="jl-button jl-button-default"><a href="<?php echo $slide->link_type; ?>"><?php echo $slide->button; ?></a>
-                        </div>
-                        <div class="jl-width-1-3">
-                            <img src="<?php echo $slide->product_image_one; ?>" alt="<?php echo $slide->title; ?>" />
-                            <?php echo $slide->new_product; ?>
-                        </div>
-                    </div>
-                </li>
-            </div>
-        <?php endforeach; ?>
-    </ul>
 </div>
